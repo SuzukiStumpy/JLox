def0e45fea6b847acdf76990e2b64efddf790566
Mark Edwards <github@medwards-online.co.uk>
2023-05-26 19:44:36 +0100

    Completed the Expr. generator to define a visitor pattern for the classes.

--- src/com/craftinginterpreters/tools/GenerateAst.java
+++ src/com/craftinginterpreters/tools/GenerateAst.java
@@ -48,8 +48,9 @@ public class GenerateAst
         writer.println();
         writer.println("import java.util.List;");
         writer.println();
-        writer.println("abstract class "+ baseName);
-        writer.println("{");
+        writer.println("abstract class "+ baseName +"{");
+
+        defineVisitor(writer, baseName, types);
 
         // The AST classes
         for (String type: types) {
@@ -58,36 +59,73 @@ public class GenerateAst
             defineType(writer, baseName, className, fields);
         }
 
+        // The base accept() method
+        writer.println();
+        writer.println("  abstract <R> R accept(Visitor<R> visitor);");
+
         writer.println("}");
         writer.close();
     }
 
+    /**
+     * Write out the concrete classes for each subtype
+     * @param writer The output stream
+     * @param baseName Base name of the Abstract parent class
+     * @param className Name of the class we are defining
+     * @param fieldList List of the fields to add to the class
+     */
     private static void defineType(
         PrintWriter writer, String baseName,
         String className, String fieldList)
     {
-        writer.println("  static class "+ className +" extends "+ baseName);
-        writer.println("  {");
-
-        // Constructor
-        writer.println("    "+ className + "("+ fieldList +")");
-        writer.println("     {");
+        writer.println("  static class "+ className +" extends "+ baseName +"{");
 
         // Store parameters in fields
         String[] fields = fieldList.split(", ");
+
+        // Fields
+        for (String field: fields) {
+            writer.println("    final "+ field +";");
+        }
+        writer.println();
+
+        // Constructor
+        writer.println("    "+ className + "("+ fieldList +") {");
+
         for (String field: fields) {
             String name = field.split(" ")[1];
             writer.println("      this."+ name +" = "+ name +";");
         }
         writer.println("    }");
 
-        // Fields
+        // Visitor pattern
         writer.println();
-        for (String field: fields) {
-            writer.println("    final "+ field +";");
-        }
+        writer.println("    @Override");
+        writer.println("    <R> R accept(Visitor<R> visitor) {");
+        writer.println("      return visitor.visit"+ className +
+            baseName + "(this);");
+        writer.println("    }");
 
         writer.println("  }");
     }
 
+    /**
+     * Write out the definition for the generic Visitor interface
+     * @param writer The output stream
+     * @param baseName Base name of the Abstract class
+     * @param types List of types to add to the interface
+     */
+    private static void defineVisitor(
+        PrintWriter writer, String baseName, List<String> types)
+    {
+        writer.println("  interface Visitor<R> {");
+
+        for (String type: types) {
+            String typeName = type.split(":")[0].trim();
+            writer.println("    R visit"+ typeName + baseName +"("+
+                typeName +" "+ baseName.toLowerCase() +");");
+        }
+
+        writer.println("    }");
+    }
 }
